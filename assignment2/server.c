6a1fff8cb3831b07d92542ddb89ff5626e2bed9e
Yi <jinyi06@gmail.com>
2022-04-20 20:56:22 -0700

    Assignment 2: commit files

---
+++ assignment2/server.c
@@ -0,0 +1,126 @@
+// Server side C/C++ program to demonstrate Socket programming
+
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <pwd.h>
+
+
+#define PORT 80
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    // char buffer[1024] = {0};
+    char *hello = "Hello from server";
+
+    struct passwd* userId;
+    pid_t childPid;
+    int status;
+
+    // Show ASLR
+    // printf("execve=0x%p\n", execve);
+
+    if (argc == 1) {       
+
+        // Creating socket file descriptor
+        if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+        {
+            perror("socket failed");
+            exit(EXIT_FAILURE);
+        }
+
+        // Attaching socket to port 80
+        // change for MacOS
+        // if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                                                    //   &opt, sizeof(opt)))
+        if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR,
+                                                    &opt, sizeof(opt)))
+        {
+            perror("setsockopt");
+            exit(EXIT_FAILURE);
+        }
+        address.sin_family = AF_INET;
+        address.sin_addr.s_addr = INADDR_ANY;
+        address.sin_port = htons( PORT );
+
+        // Forcefully attaching socket to the port
+        if (bind(server_fd, (struct sockaddr *)&address,
+                                    sizeof(address))<0)
+        {
+            perror("bind failed");
+            exit(EXIT_FAILURE);
+        }
+        if (listen(server_fd, 3) < 0)
+        {
+            perror("listen");
+            exit(EXIT_FAILURE);
+        }
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                        (socklen_t*)&addrlen))<0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+
+        childPid = fork();
+        if (childPid == 0) {
+            // pass the socket file descriptor in argument
+            char socketStr[10];
+            sprintf(socketStr, "%d", new_socket);
+            char *arg0 = (char*)argv[0];
+            char *args[] = {arg0, socketStr, NULL};
+
+            // re-exec the child process with arguments
+            status = execvp(argv[0], args);
+            if (status == -1) {
+                perror("execvp");
+                exit(EXIT_FAILURE);
+            }
+        } else if (childPid > 0) {
+            wait(NULL);
+            // printf("Parent process\n");
+        } else {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+    } else {
+        // re-execute the child process
+        // drop privilege
+        userId = getpwnam("nobody");
+        if (userId == NULL) {
+            printf("Cannot get nobody user\n");
+            exit(EXIT_FAILURE);
+        }
+        status = setuid(userId -> pw_uid);
+        if (status == 0) {
+            // read & send message
+            int dup_socket = atoi(argv[1]);
+            char buffer[1024] = {0};
+            valread = read( dup_socket , buffer, sizeof buffer);
+            if (valread < 0) {
+                perror("read");
+                exit(EXIT_FAILURE);
+            }
+            printf("%s\n",buffer );
+            if (send(dup_socket , hello , strlen(hello) , 0 ) < 0 ) {
+                perror("send");
+                exit(EXIT_FAILURE);
+            }
+            printf("Hello message sent\n");
+        } else {
+            perror("drop privileges");
+            exit(EXIT_FAILURE);
+        }
+    }
+    
+    while(wait(NULL) > 0);
+    return 0;
+}
